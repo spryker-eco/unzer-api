232d0b389c64a086200b631424651acd9074379d
Kostiantyn Kichehlov <kostiantyn.kichehlov@spryker.com>
2021-08-03 19:00:24 +0300

    СС-14341 Add base files

---
+++ src/SprykerEco/Zed/UnzerApi/UnzerApiConfig.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace SprykerEco\Zed\UnzerApi;
+
+class UnzerApiConfig
+{
+
+}
---
+++ src/SprykerEco/Zed/UnzerApi/UnzerApiDependencyProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * MIT License
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace SprykerEco\Zed\UnzerApi;
+
+use Spryker\Zed\Kernel\AbstractBundleDependencyProvider;
+use Spryker\Zed\Kernel\Container;
+use SprykerEco\Zed\AdyenApi\Dependency\Service\AdyenApiToUtilEncodingServiceBridge;
+
+class UnzerApiDependencyProvider extends AbstractBundleDependencyProvider
+{
+
+}
