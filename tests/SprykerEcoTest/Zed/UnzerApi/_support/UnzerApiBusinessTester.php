b7b44d6c26656503ce3b480600b46e3bbd4bd217
Kostiantyn Kichehlov <kostiantyn.kichehlov@spryker.com>
2021-08-04 08:59:02 +0300

    CC-14341 Add base Codeception config

---
+++ tests/SprykerEcoTest/Zed/UnzerApi/_support/UnzerApiBusinessTester.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * MIT License
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace SprykerEcoTest\Zed\UnzerApi;
+
+use Codeception\Actor;
+
+class UnzerApiBusinessTester extends Actor
+{
+
+}
